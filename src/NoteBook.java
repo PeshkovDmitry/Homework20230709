2cfe33358e9e15468586d9d0912b000c25abe1fa
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-07-09 17:31:22 +0300

    Добавлен метод для считывания данных из csv файла

--- src/NoteBook.java
+++ src/NoteBook.java
@@ -3,24 +3,137 @@ public class NoteBook {
     private String model;                   // Модель
     private String operationSystem;         // Операционная система
     private float screenDiagonal;           // Диагональ экрана
-    private int screenWidth;                // Ширина экрана в пикселях
-    private int screenHeight;               // Высота экрана в пикселях
+    private String screenResolution;        // Размер экрана в пикселях
     private String screenType;              // Тип  матрицы
-    private int screenFrequency;            // Частота обновления экрана
     private String processorModel;          // Модель процессора
     private int processorCoreNumber;        // Количество ядер процессора
     private float processorCoreFrequency;   // Тактовая частота процессора
     private int ramSize;                    // Размер оперативной памяти
     private String ramType;                 // Тип оперативной памяти
-    private int ramFrequency;               // Частота оперативной памяти
     private String integratedVideoType;     // Тип встроенной видеокарты
     private String discreteVideoType;       // Тип дискретной видеокарты
     private int ssdSize;                    // Размер SSD диска
-    private int ssdConnectionType;          // Тип подключения SSD диска
-    private int hddSize;                    // Размер HDD диска
-    private int hddConnectionType;          // Тип подключения HDD диска
     private int price;                      // Цена
-    private int priceWithoutDiscount;       // Цена без скидки
 
 
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getOperationSystem() {
+        return operationSystem;
+    }
+
+    public void setOperationSystem(String operationSystem) {
+        this.operationSystem = operationSystem;
+    }
+
+    public float getScreenDiagonal() {
+        return screenDiagonal;
+    }
+
+    public void setScreenDiagonal(float screenDiagonal) {
+        this.screenDiagonal = screenDiagonal;
+    }
+
+    public String getScreenType() {
+        return screenType;
+    }
+
+    public void setScreenType(String screenType) {
+        this.screenType = screenType;
+    }
+
+    public String getProcessorModel() {
+        return processorModel;
+    }
+
+    public void setProcessorModel(String processorModel) {
+        this.processorModel = processorModel;
+    }
+
+    public int getProcessorCoreNumber() {
+        return processorCoreNumber;
+    }
+
+    public void setProcessorCoreNumber(int processorCoreNumber) {
+        this.processorCoreNumber = processorCoreNumber;
+    }
+
+    public float getProcessorCoreFrequency() {
+        return processorCoreFrequency;
+    }
+
+    public void setProcessorCoreFrequency(float processorCoreFrequency) {
+        this.processorCoreFrequency = processorCoreFrequency;
+    }
+
+    public int getRamSize() {
+        return ramSize;
+    }
+
+    public void setRamSize(int ramSize) {
+        this.ramSize = ramSize;
+    }
+
+    public String getRamType() {
+        return ramType;
+    }
+
+    public void setRamType(String ramType) {
+        this.ramType = ramType;
+    }
+
+    public String getIntegratedVideoType() {
+        return integratedVideoType;
+    }
+
+    public void setIntegratedVideoType(String integratedVideoType) {
+        this.integratedVideoType = integratedVideoType;
+    }
+
+    public String getDiscreteVideoType() {
+        return discreteVideoType;
+    }
+
+    public void setDiscreteVideoType(String discreteVideoType) {
+        this.discreteVideoType = discreteVideoType;
+    }
+
+    public int getSsdSize() {
+        return ssdSize;
+    }
+
+    public void setSsdSize(int ssdSize) {
+        this.ssdSize = ssdSize;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+    public String getScreenResolution() {
+        return screenResolution;
+    }
+
+    public void setScreenResolution(String screenResolution) {
+        this.screenResolution = screenResolution;
+    }
 }
--- src/NoteBookData.java
+++ src/NoteBookData.java
@@ -1,2 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
 public class NoteBookData {
+    List<NoteBook> list = new LinkedList<>();
+
+    /**
+     * Метод для считывания и парсинга имеющегося файла с данными ноутбуков
+     */
+    public void loadFromFile() {
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("notebooks.csv"));
+            // Считываем первую строку с заголовками таблицы
+            String str = bufferedReader.readLine();
+            // Считывыаем данные
+            while ((str = bufferedReader.readLine()) != null) {
+                NoteBook nb = new NoteBook();
+                String[] data = str.split(";");
+                nb.setManufacturer(data[0]);
+                nb.setModel(data[1]);
+                nb.setPrice(Integer.parseInt(data[2]));
+                nb.setProcessorModel(data[3]);
+                nb.setProcessorCoreNumber(Integer.parseInt(data[4]));
+                nb.setProcessorCoreFrequency(Float.parseFloat(data[5]));
+                nb.setDiscreteVideoType(data[6]);
+                nb.setIntegratedVideoType(data[7]);
+                nb.setRamSize(Integer.parseInt(data[8]));
+                nb.setRamType(data[9]);
+                nb.setSsdSize(Integer.parseInt(data[10]));
+                nb.setScreenResolution(data[11]);
+                nb.setScreenDiagonal(Float.parseFloat(data[12]));
+                nb.setScreenType(data[13]);
+                nb.setOperationSystem(data[14]);
+                System.out.println(nb);
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
 }
