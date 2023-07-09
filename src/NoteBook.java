1707bb945c2ca14310b3d4bdcf0e4f676e6f4e6f
PeshkovDmitry <peshkov-dv@yandex.ru>
2023-07-09 15:24:33 +0300

    Начальный коммит. Создание пустых файлов для классов. Поля для класса Notebook

---
+++ src/NoteBook.java
@@ -0,0 +1,26 @@
+public class NoteBook {
+    private String manufacturer;            // Производитель
+    private String model;                   // Модель
+    private String operationSystem;         // Операционная система
+    private float screenDiagonal;           // Диагональ экрана
+    private int screenWidth;                // Ширина экрана в пикселях
+    private int screenHeight;               // Высота экрана в пикселях
+    private String screenType;              // Тип  матрицы
+    private int screenFrequency;            // Частота обновления экрана
+    private String processorModel;          // Модель процессора
+    private int processorCoreNumber;        // Количество ядер процессора
+    private float processorCoreFrequency;   // Тактовая частота процессора
+    private int ramSize;                    // Размер оперативной памяти
+    private String ramType;                 // Тип оперативной памяти
+    private int ramFrequency;               // Частота оперативной памяти
+    private String integratedVideoType;     // Тип встроенной видеокарты
+    private String discreteVideoType;       // Тип дискретной видеокарты
+    private int ssdSize;                    // Размер SSD диска
+    private int ssdConnectionType;          // Тип подключения SSD диска
+    private int hddSize;                    // Размер HDD диска
+    private int hddConnectionType;          // Тип подключения HDD диска
+    private int price;                      // Цена
+    private int priceWithoutDiscount;       // Цена без скидки
+
+
+}
